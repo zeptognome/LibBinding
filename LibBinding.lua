3309d7d24084e359c61cfc862bd6ca555f9b62a8
zeptognome <zeptognome@gmail.com>
2024-08-10 13:58:31 -0700

    more cleanup
    Signed-off-by: zeptognome <zeptognome@gmail.com>

--- LibBinding.lua
+++ LibBinding.lua
@@ -1,9 +1,4 @@
---[[ 
-LibBinding
-Copyright (c) 2024 zeptognome
-Functions to determine current binding of items 
-]]--
-
+---- LibBinding ---
 assert(LibStub, "LibStub not found.");
 local major, minor = "LibBinding", 1;
 
@@ -12,8 +7,22 @@ local LibBinding = LibStub:NewLibrary(major, minor);
 
 if not LibBinding then return end;
 
----@enum BindingType -- note these are similar, but NOT the same as Enum.ItemBind
-LibBinding.BindingType = {
+---@enum Binding
+LibBinding.BINDING = {
+  NONE = "None",
+  SOULBOUND = "Soulbound",
+  BOE = "BoE",
+  BOU = "BoU",
+  QUEST = "Quest",
+  NOBANK = "NoBank",
+  UNKNOWN = "Unknown",
+  ACCOUNT = "Warbound",
+  BNET = "BNet",
+  WUE = "WuE",
+}
+
+---@enum BindingID -- note these are similar, but NOT the same as Enum.ItemBind
+LibBinding.BINDINGID = {
   NONE = 0,
   SOULBOUND = 1,
   BOE = 2,
@@ -26,43 +35,94 @@ LibBinding.BindingType = {
   WUE = 9,
 }
 
+---@class TradableMap
+---@type table<BindingID|Binding|string, boolean>
+LibBinding.TradableMap = {
+  [LibBinding.BINDINGID.NONE] = true,
+  [LibBinding.BINDING.NONE] = true,
+  [""] = true,
+  [LibBinding.BINDINGID.SOULBOUND] = false,
+  [LibBinding.BINDING.SOULBOUND] = false,
+  [ITEM_SOULBOUND] = false,
+  [LibBinding.BINDINGID.BOE] = true,
+  [LibBinding.BINDING.BOE] = true,
+  [ITEM_BIND_ON_EQUIP] = true,
+  [LibBinding.BINDINGID.BOU] = true,
+  [LibBinding.BINDING.BOU] = true,
+  [ITEM_BIND_ON_USE] = true,
+  [LibBinding.BINDINGID.QUEST] = false,
+  [LibBinding.BINDING.QUEST] = false,
+  [ITEM_BIND_QUEST] = false,
+  [LibBinding.BINDINGID.NOBANK] = false,
+  [LibBinding.BINDING.NOBANK] = false,
+  [LibBinding.BINDINGID.UNKNOWN] = false,
+  [LibBinding.BINDING.UNKNOWN] = false,
+  [ERROR_CAPS] = false,
+  [LibBinding.BINDINGID.ACCOUNT] = false,
+  [LibBinding.BINDING.ACCOUNT] = false,
+  [ITEM_ACCOUNTBOUND] = false,
+  [LibBinding.BINDINGID.BNET] = false,
+  [LibBinding.BINDING.BNET] = false,
+  [ITEM_BNETACCOUNTBOUND] = false,
+  [LibBinding.BINDINGID.WUE] = false,
+  [LibBinding.BINDING.WUE] = false,
+  [ITEM_ACCOUNTBOUND_UNTIL_EQUIP] = false,
+}
+
+---@class BindingStringMAP
+---@type table<Binding, BindingID>
+LibBinding.BINDINGSTRINGMAP = {
+  [LibBinding.BINDING.NONE] = LibBinding.BINDINGID.NONE,
+  [LibBinding.BINDING.SOULBOUND] = LibBinding.BINDINGID.SOULBOUND,
+  [LibBinding.BINDING.BOE] = LibBinding.BINDINGID.BOE,
+  [LibBinding.BINDING.BOU] = LibBinding.BINDINGID.BOU,
+  [LibBinding.BINDING.QUEST] = LibBinding.BINDINGID.QUEST,
+  [LibBinding.BINDING.NOBANK] = LibBinding.BINDINGID.NOBANK,
+  [LibBinding.BINDING.UNKNOWN] = LibBinding.BINDINGID.UNKNOWN,
+  [LibBinding.BINDING.ACCOUNT] = LibBinding.BINDINGID.ACCOUNT,
+  [LibBinding.BINDING.BNET] = LibBinding.BINDINGID.BNET,
+  [LibBinding.BINDING.WUE] = LibBinding.BINDINGID.WUE,
+}
+
 ---@enum BindingString
-LibBinding.BindingString = {
- [LibBinding.BindingType.NONE] = "None",
- [LibBinding.BindingType.SOULBOUND] = "Soulbound",
- [LibBinding.BindingType.BOE] = "BoE",
- [LibBinding.BindingType.BOU] = "BoU",
- [LibBinding.BindingType.QUEST] = "Quest",
- [LibBinding.BindingType.NOBANK] = "NoBank",
- [LibBinding.BindingType.UNKNOWN] = "Unknown",
- [LibBinding.BindingType.ACCOUNT] = "Warbound",
- [LibBinding.BindingType.BNET] = "BNet",
- [LibBinding.BindingType.WUE] = "WuE",
+---@type table<BindingID, Binding>
+LibBinding.BINDINGSTRING = {
+ [LibBinding.BINDINGID.NONE] = LibBinding.BINDING.NONE,
+ [LibBinding.BINDINGID.SOULBOUND] = LibBinding.BINDING.SOULBOUND,
+ [LibBinding.BINDINGID.BOE] = LibBinding.BINDING.BOE,
+ [LibBinding.BINDINGID.BOU] = LibBinding.BINDING.BOU,
+ [LibBinding.BINDINGID.QUEST] = LibBinding.BINDING.QUEST,
+ [LibBinding.BINDINGID.NOBANK] = LibBinding.BINDING.NOBANK,
+ [LibBinding.BINDINGID.UNKNOWN] = LibBinding.BINDING.UNKNOWN,
+ [LibBinding.BINDINGID.ACCOUNT] = LibBinding.BINDING.ACCOUNT,
+ [LibBinding.BINDINGID.BNET] = LibBinding.BINDING.BNET,
+ [LibBinding.BINDINGID.WUE] = LibBinding.BINDING.WUE,
 }
 
 ---@enum GlobalString
-LibBinding.GlobalString = {
-  [LibBinding.BindingType.NONE] = "",
-  [LibBinding.BindingType.SOULBOUND] = ITEM_SOULBOUND,
-  [LibBinding.BindingType.BOE] = ITEM_BIND_ON_EQUIP,
-  [LibBinding.BindingType.BOU] = ITEM_BIND_ON_USE,
-  [LibBinding.BindingType.QUEST] = ITEM_BIND_QUEST,
-  [LibBinding.BindingType.NOBANK] = ERROR_CAPS,
-  [LibBinding.BindingType.UNKNOWN] = ERROR_CAPS,
-  [LibBinding.BindingType.ACCOUNT] = ITEM_ACCOUNTBOUND,
-  [LibBinding.BindingType.BNET] = ITEM_BNETACCOUNTBOUND,
-  [LibBinding.BindingType.WUE] = ITEM_ACCOUNTBOUND_UNTIL_EQUIP,
+---@type table<BindingID, string>
+LibBinding.GLOBALSTRING = {
+  [LibBinding.BINDINGID.NONE] = "",
+  [LibBinding.BINDINGID.SOULBOUND] = ITEM_SOULBOUND,
+  [LibBinding.BINDINGID.BOE] = ITEM_BIND_ON_EQUIP,
+  [LibBinding.BINDINGID.BOU] = ITEM_BIND_ON_USE,
+  [LibBinding.BINDINGID.QUEST] = ITEM_BIND_QUEST,
+  [LibBinding.BINDINGID.NOBANK] = ERROR_CAPS,
+  [LibBinding.BINDINGID.UNKNOWN] = ERROR_CAPS,
+  [LibBinding.BINDINGID.ACCOUNT] = ITEM_ACCOUNTBOUND,
+  [LibBinding.BINDINGID.BNET] = ITEM_BNETACCOUNTBOUND,
+  [LibBinding.BINDINGID.WUE] = ITEM_ACCOUNTBOUND_UNTIL_EQUIP,
 }
 
 ---@param itemLocation ItemLocationMixin
----@return Enum.ItemBind
+---@return BindingID
 function LibBinding.FetchBindType(itemLocation)
-  local bindType
+  local bindType = nil -- force the assert
   local item = Item:CreateFromItemLocation(itemLocation)
-  local itemCallback = function()
+  if item.IsItemDataCached then
     bindType,_,_,_ = select(14, C_Item.GetItemInfo(item:GetItemID()))
   end
-  item:ContinueOnItemLoad(itemCallback)
+    assert(bindType, "Uncached data")
   return bindType
 end
 
@@ -77,13 +137,17 @@ end
 ---@param itemLocation ItemLocationMixin
 ---@return boolean
 function LibBinding.isNontradable(itemLocation)
-  return C_Item.IsBound(itemLocation) or C_Item.IsBoundToAccountUntilEquip(itemLocation)
+  return C_Item.IsBound(itemLocation) or LibBinding.isWarboundUntilEquipped(itemLocation)
 end
 
 ---@param itemLocation ItemLocationMixin
 ---@return boolean
 function LibBinding.isTradable(itemLocation)
-  return not C_Item.IsBound(itemLocation) and not C_Item.IsBoundToAccountUntilEquip(itemLocation)
+  local wue = false
+  if ( C_Item.IsBoundToAccountUntilEquip and C_Item.IsBoundToAccountUntilEquip(itemLocation)) then
+    wue = true
+  end
+  return not (wue or C_Item.IsBound(itemLocation))
 end
 
 ---@param itemLocation ItemLocationMixin
@@ -116,54 +180,74 @@ end
 function LibBinding.isSoulbound(itemLocation, bindType)
   local type = bindType or LibBinding.FetchBindType(itemLocation)
   return C_Item.IsBound(itemLocation)
-    and not LibBinding.isQuestbound(itemLocation,type)
-    and not C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation)
+    and not LibBinding.isQuestbound(itemLocation,type --[[@as Enum.ItemBind]])
+    and not (C_Bank and C_Bank.IsItemAllowedInBankType and C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation))
 end
 
 ---@param itemLocation ItemLocationMixin
 ---@return boolean
 function LibBinding.isWarbound(itemLocation)
-  return C_Item.IsBound(itemLocation) and C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation)
+  local accountbankable = false
+  if ( C_Bank and C_Bank.IsItemAllowedInBankType and C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation)) then
+    accountbankable = true
+  end
+  return C_Item.IsBound(itemLocation) and accountbankable
 end
 
 ---@param itemLocation ItemLocationMixin
 ---@return boolean
 function LibBinding.isWarboundUntilEquipped(itemLocation)
-  return C_Item.IsBoundToAccountUntilEquip(itemLocation) and not C_Item.IsBound(itemLocation)
+  local wue = false
+  if ( C_Item.IsBoundToAccountUntilEquip and C_Item.IsBoundToAccountUntilEquip(itemLocation)) then
+    wue = true
+  end
+  return not C_Item.IsBound(itemLocation) and wue
 end
 
 ---@param itemLocation ItemLocationMixin
----@param bindType? Enum.ItemBind
----@return BindingType
+---@param bindType Enum.ItemBind
+---@return BindingID
 function LibBinding.ParseBoundTypes(itemLocation, bindType)
-  local type = 1  -- Assume Soulbound
-  if C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation) then
-    type = 7  -- Assign AccountBound
+  local type = LibBinding.BINDINGID.SOULBOUND  -- Assume Soulbound
+  if C_Bank and C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation) then
+    type = LibBinding.BINDINGID.ACCOUNT  -- Assign AccountBound
   end
   if bindType == 4 then
-    type = 4   -- Quest type overrides all other bindings
+    type = LibBinding.BINDINGID.QUEST   -- Quest type overrides all other bindings
   end
   return type
 end
 
 ---@param itemLocation ItemLocationMixin
----@param bindType? Enum.ItemBind
----@return BindingType
+---@param bindType Enum.ItemBind
+---@return BindingID
 function LibBinding.GetItemBinding(itemLocation, bindType)
   -- assume synthetictype passed correctly or default to a GetItemInfo lookup
-  local synthetictype = bindType or LibBinding.FetchBindType(itemLocation) --[[@as BindingType]]
+  local synthetictype = bindType --[[@as BindingID]]
 
   if C_Item.IsBound(itemLocation) then  -- Types Soulbound(1), Quest(4), and Accountbound(7) should match
-    synthetictype = LibBinding.ParseBoundTypes(itemLocation, synthetictype)
+    synthetictype = LibBinding.ParseBoundTypes(itemLocation, synthetictype --[[@as Enum.ItemBind]])
   end
 
-  if C_Item.IsBoundToAccountUntilEquip(itemLocation) then
-    synthetictype = 9   -- override Warbound BoEs from type BoE(2) to type WuE(9)
+  if C_Item.IsBoundToAccountUntilEquip and C_Item.IsBoundToAccountUntilEquip(itemLocation) then
+    synthetictype = LibBinding.BINDINGID.WUE   -- override Warbound BoEs from type BoE(2) to type WuE(9)
   end
 
-  if LibBinding.isTradable (itemLocation) and not C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation) then
-    synthetictype = 5  -- this shouldn't happen, something has changed as of 11.0, only quest(4) and soulbound(1) items are blocked from account bank
+  if LibBinding.isTradable (itemLocation) and C_Bank and C_Bank.IsItemAllowedInBankType and not C_Bank.IsItemAllowedInBankType(Enum.BankType.Account, itemLocation) then
+    synthetictype = LibBinding.BINDINGID.NOBANK  -- this shouldn't happen, something has changed as of 11.0, only quest(4) and soulbound(1) items are blocked from account bank
   end
 
   return synthetictype -- types None(0), BoE(2), BoU(3) plus any types we don't see such as unused(6) or BNet(8)
+end
+
+---@param binding Binding|BindingID|string
+---@return boolean
+function LibBinding.isBindingTradable(binding) -- mostly here for future compat if Enum.ItemBind changes
+  return LibBinding.TradableMap[binding]
+end
+
+---@param binding Binding|BindingID|string
+---@return boolean
+function LibBinding.isBindingNontradable(binding)
+  return LibBinding.TradableMap[binding]
 end
